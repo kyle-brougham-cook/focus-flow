183a01ebe05d086203829613e2beb1c31f32fe2c
kyle <kylebroughamcook@gmail.com>
2026-01-26 16:37:16 +0200

    ui: improved upon intital task skeleton

--- client/src/components/TaskComponentSkeleton.tsx
+++ client/src/components/TaskComponentSkeleton.tsx
@@ -4,22 +4,22 @@ const TaskComponentSkeleton = () => {
       className={`shadow-md bg-gray-50 ring-1 ring-violet-300 rounded-xl transition-shadow w-265px h-178px max-w-[265px] flex flex-col`}
     >
       <h3
-        className={`pl-1 pr-1`}
+        className="animate-pulse pl-1 pr-1 h-[32px]"
       >
       </h3>
-      <p className="h-20 mb-4 p-1">
+      <p className="h-20 mb-4 p-1 animate-pulse ">
       </p>
       <div className="flex flex-col sm:flex-row justify-center gap-2 w-full mt-2 p-2">
         <button
-          className="h-[24px] w-[75.672px] flex-1 border border-violet-600 rounded transition"
+          className="animate-pulse h-[24px] w-[75.672px] flex-1 border border-violet-600 rounded transition"
          >
         </button>
         <button
-          className="h-[24px] w-[75.672px] flex-1 border border-violet-600 rounded transition"
+          className="animate-pulse h-[24px] w-[75.672px] flex-1 border border-violet-600 rounded transition"
          >
         </button>
         <button 
-           className="h-[24px] w-[75.672px] flex-1 border border-violet-600 rounded transition"
+           className="animate-pulse h-[24px] w-[75.672px] flex-1 border border-violet-600 rounded transition"
           >
         </button>
       </div>
