875c946c77a159b01ac006fdcb2f840439326d9b
kyle <kylebroughamcook@gmail.com>
2026-01-28 11:57:00 +0200

    ui: improved task skeleton to contain header/description placeholder

--- client/src/components/TaskComponentSkeleton.tsx
+++ client/src/components/TaskComponentSkeleton.tsx
@@ -1,27 +1,13 @@
 const TaskComponentSkeleton = () => {
      return (
     <div
-      className={`shadow-md bg-gray-50 ring-1 ring-violet-300 rounded-xl transition-shadow w-265px h-178px max-w-[265px] flex flex-col`}
+      className={`bg-violet-400/20 max-w-sm w-full mx-auto shadow-md ring-1 ring-violet-300 rounded-xl transition-shadow h-[178px] flex flex-col`}
     >
-      <h3
-        className="animate-pulse pl-1 pr-1 h-[32px]"
+      <div
+        className="self-center animate-pulse bg-white w-[60%] h-[30px] mt-4 rounded"
       >
-      </h3>
-      <p className="h-20 mb-4 p-1 animate-pulse ">
-      </p>
-      <div className="flex flex-col sm:flex-row justify-center gap-2 w-full mt-2 p-2">
-        <button
-          className="animate-pulse h-[24px] w-[75.672px] flex-1 border border-violet-600 rounded transition"
-         >
-        </button>
-        <button
-          className="animate-pulse h-[24px] w-[75.672px] flex-1 border border-violet-600 rounded transition"
-         >
-        </button>
-        <button 
-           className="animate-pulse h-[24px] w-[75.672px] flex-1 border border-violet-600 rounded transition"
-          >
-        </button>
+      </div>
+      <div className="self-center animate-pulse bg-white/50 w-[60%] h-[60px] mt-4 rounded">
       </div>
     </div>
   );
